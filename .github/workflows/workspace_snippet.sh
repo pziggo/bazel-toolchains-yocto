fbed675c0c4cfb8147dab311907ad1877c7abe5d
Patrick Ziegler <ziggo@zaebos.de>
2022-10-09 20:50:00 +0200

    github: Create release workflow

---
+++ .github/workflows/workspace_snippet.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Set by GH actions, see
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+TAG=${GITHUB_REF_NAME}
+PREFIX="bazel-toolchains-yocto-${TAG:1}"
+SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
+
+cat << EOF
+WORKSPACE snippet:
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "bazel_toolchains_yocto",
+    sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
+    url = "https://github.com/pziggo/bazel-toolchains-yocto/archive/refs/tags/${TAG}.tar.gz",
+)
+
+load("@bazel_toolchains_yocto//toolchains:repositories.bzl", "bazel_toolchains_yocto_dependencies")
+
+bazel_toolchains_yocto_dependencies()
+\`\`\`
+EOF
--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 Bazel integration for toolchains as built by the [Yocto Project](https://www.yoctoproject.org).
 You can find the documentation under [docs](./docs/).
 
+_Repository structure and GitHub workflows based on [bazel-contrib/rules-template](https://github.com/bazel-contrib/rules-template)_
+
 ## Installation
 
 Installation instructions are included on each release:
